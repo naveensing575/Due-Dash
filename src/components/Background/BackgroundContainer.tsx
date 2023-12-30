fa586e02111bb23b879438df62b54c012d317e73
naveen.singh <naveen.singh@itelasoft.com>
2023-12-30 17:17:49 +0530

    removed audio

--- src/components/Background/BackgroundContainer.tsx
+++ src/components/Background/BackgroundContainer.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Video from '../Video/Video'
-import Audio from '../Audio/Audio'
 
 const BackgroundContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Video />
-      <Audio />
       {children}
     </>
   )
